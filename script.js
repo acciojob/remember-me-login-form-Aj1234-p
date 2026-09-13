4b3aa8484926020e7f04728fdef3ff451ea03d8a
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-09-13 17:24:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ let checkBox = document.querySelector('#checkbox');
 let loginButton = document.querySelector('#existing');
 let formContainer = document.querySelector('#form-container');
 
+
+
+
 let newUser, newPassword, userDetails = [];
+
+if(JSON.parse(localStorage.getItem("User Details "))!==null){
+  loginButton.style.display = 'inline';
+}
 submitButton.addEventListener('click',(e)=>{
   e.preventDefault();
   newUser = userName.value, newPassword = passWord.value;
@@ -14,23 +21,28 @@ submitButton.addEventListener('click',(e)=>{
   console.log("password ",newPassword);
    alert(`Logged in as ${newUser}`);
    if(checkBox.checked){
+       console.log("when check box is checked");
        let isExist = false;
        let existDetails = JSON.parse(localStorage.getItem("User Details ")) || [];
        for(let i=0;i<existDetails.length;i++){
         let user = existDetails[i];
-        if(user.name===newUser){
+        if(user.name===newUser && user.pass === newPassword){
           isExist = true;
           loginButton.style.display = 'inline';
         }
        }
        if(!isExist){
+        console.log("user first time");
          userDetails.push({name: newUser, pass:newPassword})
          localStorage.setItem("User Details ",JSON.stringify(userDetails));
        }
-      }else{
+      } else{
+        console.log("when check box not to be checked");
         if(JSON.parse(localStorage.getItem("User Details "))!==null){
         let fetchDetails = JSON.parse(localStorage.getItem("User Details "));
-        let updateDetails = fetchDetails.filter((user)=> user.name!==newUser);
+        console.log("fetch details ",fetchDetails);
+        let updateDetails = fetchDetails.filter((user)=> user.name!==newUser && user.pass!==newPassword);
+        console.log("update details ",updateDetails);
         localStorage.clear();
         localStorage.setItem("User Details ",JSON.stringify(updateDetails));
        }
