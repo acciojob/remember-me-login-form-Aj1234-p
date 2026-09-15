b5a43f81edad40e8801f176f3c42b755015d6e69
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-09-15 16:59:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 let submitButton = document.querySelector('#submit');
 let userName = document.querySelector('#username');
 let passWord = document.querySelector('#password');
@@ -20,7 +19,7 @@ submitButton.addEventListener('click',(e)=>{
    if(checkBox.checked){
        console.log("when check box is checked");
        let isExist = false;
-       let existDetails = JSON.parse(localStorage.getItem("User Details ")) || [];
+       let existDetails = JSON.parse(localStorage.getItem("User Details")) || [];
        for(let i=0;i<existDetails.length;i++){
         let user = existDetails[i];
         if(user.name===newUser && user.pass === newPassword){
@@ -39,8 +38,8 @@ submitButton.addEventListener('click',(e)=>{
     } 
      else{
         console.log("when check box not to be checked");
-        if(JSON.parse(localStorage.getItem("User Details "))!==null){
-        let fetchDetails = JSON.parse(localStorage.getItem("User Details "));
+        if(JSON.parse(localStorage.getItem("User Details"))!==null){
+        let fetchDetails = JSON.parse(localStorage.getItem("User Details"));
         console.log("fetch details ",fetchDetails);
         let updateDetails = fetchDetails.filter((user)=> user.name!==newUser && user.pass!==newPassword);
         console.log("update details",updateDetails);
@@ -53,7 +52,7 @@ submitButton.addEventListener('click',(e)=>{
 
 loginButton.addEventListener('click',(e)=>{
   e.preventDefault();
-  let savedName = JSON.parse(localStorage.getItem("User Details "));
+  let savedName = JSON.parse(localStorage.getItem("User Details"));
   console.log("Saved name",savedName);
   alert(`Logged in as ${savedName[savedName.length-1].name}`);
   console.log("login button")
